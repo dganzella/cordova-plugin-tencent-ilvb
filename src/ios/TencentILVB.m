18fca13c561e76159a8d914769e295955c0a60dd
Danilo Ganzella <dganzella@2mundos.net>
2017-10-31 18:35:25 -0200

    Added debugs and implemented endpoints.

--- src/ios/TencentILVB.m
+++ src/ios/TencentILVB.m
@@ -4,19 +4,18 @@
 {
     UIView * videosview;
     NSString * callbackId;
-    NSMutableDictionary *viewsPositions;
-
 }
 
 -(void) pluginInitialize
 {
-    viewsPositions = [[NSMutableDictionary alloc] init];
-    
+    NSLog(@"ILVB initialized");
+
     videosview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [[UIScreen mainScreen] bounds].size.width, [[UIScreen mainScreen] bounds].size.height)];
     [videosview setBackgroundColor:[UIColor clearColor]];
 }
 - (void)addEvent:(CDVInvokedUrlCommand*)command
 {
+    NSLog(@"Add event with success");
     callbackId = command.callbackId;
 }
 
@@ -35,20 +34,52 @@
 
 - (BOOL)onEndpointsUpdateInfo:(QAVUpdateEvent)event updateList:(NSArray *)endpoints
 {
-
+    NSLog(@"ENDPOINTS UPDATE INFO RECEIVED");
+    
+    for (NSString* openid in endpoints)
+    {
+        NSMutableDictionary* eventData = [[NSMutableDictionary alloc] init];
+        [eventData setObject:openid forKey:@"openid"];
+        
+        if(event == QAV_EVENT_ID_ENDPOINT_HAS_CAMERA_VIDEO)
+        {
+            if([openid isEqualToString: [ILiveLoginManager getInstance].getLoginId])
+            {
+                [self triggerJSEvent: @"onLocalStreamAdd" withData: eventData];
+            }
+            else
+            {
+                [self triggerJSEvent: @"onUpdateRemoteStream" withData: eventData];
+            }
+        }
+        else if(event == QAV_EVENT_ID_ENDPOINT_NO_CAMERA_VIDEO)
+        {
+            if(![openid isEqualToString: [ILiveLoginManager getInstance].getLoginId])
+            {
+                [self triggerJSEvent: @"onRemoteStreamRemove" withData: eventData];
+            }
+        }
+    }
+    
     return false;
 }
 
 - (void)init:(CDVInvokedUrlCommand*)command
 {
     NSString* sdkppid = [[command arguments] objectAtIndex:0];
+    NSLog(@"SDK APP ID: %@", sdkppid);
+    
     NSString* accounttype = [[command arguments] objectAtIndex:1];
+    NSLog(@"ACCOUNT TYPE: %@", accounttype);
     
     [[ILiveSDK getInstance] initSdk:[sdkppid intValue] accountType:[accounttype intValue]];
     [[ILiveSDK getInstance] setConsoleLogPrint:YES];
     
     NSString* openid = [[command arguments] objectAtIndex:2];
+    NSLog(@"OPEN ID: %@", openid);
+    
     NSString* usersig = [[command arguments] objectAtIndex:3];
+    NSLog(@"USER SIG: %@", usersig);
     
     [[ILiveLoginManager getInstance] iLiveLogin:openid sig:usersig succ:^{
         
@@ -70,13 +101,24 @@
 
 - (void)createRoom:(CDVInvokedUrlCommand*)command
 {
+    NSLog(@"CREATE ROOM");
     
     int roomId = [[[command arguments] objectAtIndex:0] intValue];
+    
+    NSLog(@"ROOM ID: %d", roomId);
+    
     NSString*  role = [[command arguments] objectAtIndex:1];
+    
+    NSLog(@"ROLE: %@", role);
+    
     NSString*  hostId = [[command arguments] objectAtIndex:2];
+    
+    NSLog(@"HOST ID: %@", hostId);
     
     if([role isEqualToString:@"LiveMaster"])
     {
+        NSLog(@"IS HOST, WILL CREATE ROOM");
+        
         TILLiveRoomOption *option = [TILLiveRoomOption defaultHostLiveOption];
         option.imOption.imSupport = NO;
         option.avOption.autoCamera = true;
@@ -94,6 +136,8 @@
     }
     else
     {
+        NSLog(@"IS VIEWER, WILL JOIN ROOM");
+        
         TILLiveRoomOption *option = [TILLiveRoomOption defaultGuestLiveOption];
         option.imOption.imSupport = NO;
         option.avOption.autoCamera = true;
@@ -114,7 +158,11 @@
 
 - (void)updateView:(CDVInvokedUrlCommand*)command
 {
+    NSLog(@"UPDATE VIEW");
+    
     NSString* openid = [[command arguments] objectAtIndex:0];
+    
+    NSLog(@"OPEN ID: %@", openid);
     
     int top = [[[command arguments] objectAtIndex:1] intValue];
     int left = [[[command arguments] objectAtIndex:2] intValue];
@@ -122,11 +170,19 @@
     int height = [[[command arguments] objectAtIndex:4] intValue];
     double ratio = [[[command arguments] objectAtIndex:0] doubleValue];
     
+    NSLog(@"TOP: %d", top);
+    NSLog(@"LEFT: %d", left);
+    NSLog(@"WIDTH: %d", width);
+    NSLog(@"HEIGHT: %d", height);
+    NSLog(@"RATIO: %f", ratio);
+    
     [[TILLiveManager getInstance] addAVRenderView: CGRectMake((int)(left*ratio), (int)(top*ratio), (int)(width*ratio), (int)(height*ratio)) forIdentifier:openid srcType: QAVVIDEO_SRC_TYPE_CAMERA];
 }
 
 - (void)quit:(CDVInvokedUrlCommand*)command
 {
+    NSLog(@"QUIT");
+    
     TILLiveManager *manager = [TILLiveManager getInstance];
 
     [manager quitRoom:^{
