9b88ac0f998dbc3a20734476954c68af9ed0c094
Danilo Ganzella <dganzella@2mundos.net>
2017-10-31 17:31:21 -0200

    Start implementing the iOS Tencent.

--- src/ios/TencentILVB.h
+++ src/ios/TencentILVB.h
@@ -1,7 +1,21 @@
 #import <Cordova/CDV.h>
+#import <ImSDK/ImSDK.h>
+#import <QAVSDK/QAVSDK.h>
+#import <QALSDK/QalSDKProxy.h>
+#import <TLSSDK/TLSHelper.h>
+#import <IMSDKBugly/IMSDKBugly.h>
+
+#import <ILiveSDK/ILiveCoreHeader.h>
+#import <ILiveSDK/ILiveQualityData.h>
+#import <ILiveSDK/ILiveSpeedTestManager.h>
+#import <TILLiveSDK/TILLiveSDK.h>
 
 @interface TencentILVB : CDVPlugin
 
-- (void) greet:(CDVInvokedUrlCommand*)command;
+- (void)init:(CDVInvokedUrlCommand*)command;
+- (void)createRoom:(CDVInvokedUrlCommand*)command;
+- (void)updateViews:(CDVInvokedUrlCommand*)command;
+- (void)quit:(CDVInvokedUrlCommand*)command;
 
 @end
+
--- src/ios/TencentILVB.m
+++ src/ios/TencentILVB.m
@@ -2,17 +2,69 @@
 
 @implementation TencentILVB
 
-- (void)greet:(CDVInvokedUrlCommand*)command
+- (void)init:(CDVInvokedUrlCommand*)command
 {
-
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    NSString* sdkppid = [[command arguments] objectAtIndex:0];
+    NSString* accounttype = [[command arguments] objectAtIndex:1];
+    
+    [[ILiveSDK getInstance] initSdk:[sdkppid intValue] accountType:[accounttype intValue]];
+    [[ILiveSDK getInstance] setConsoleLogPrint:YES];
+    
+    NSString* openid = [[command arguments] objectAtIndex:2];
+    NSString* usersig = [[command arguments] objectAtIndex:3];
+    
+    [[ILiveLoginManager getInstance] iLiveLogin:openid sig:usersig succ:^{
+        
+        NSLog(@"Logged in successfully");
+        
+        CDVPluginResult* result = [CDVPluginResult
+                                   resultWithStatus:CDVCommandStatus_OK
+                                   messageAsString:@"Logged Successfully"];
+        
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        
+    } failed:^(NSString *moudle, int errId, NSString *errMsg) {
+        NSLog(@"Login failed");
+    }];
 }
+
+
+- (void)createRoom:(CDVInvokedUrlCommand*)command
+{
+    
+    int roomId = [[[command arguments] objectAtIndex:0] intValue];
+    NSString*  role = [[command arguments] objectAtIndex:1];
+    NSString*  hostId = [[command arguments] objectAtIndex:2];
+    
+    if([role isEqualToString:@"LiveMaster"])
+    {
+        TILLiveRoomOption *option = [TILLiveRoomOption defaultHostLiveOption];
+        option.imOption.imSupport = NO;
+        
+        
+        [[TILLiveManager getInstance] createRoom:roomId option:option succ:^{
+            
+            NSLog(@"createRoom succ");
+            
+            
+        } failed:^(NSString *module, int errId, NSString *errMsg) {
+            
+            NSString *errinfo = [NSString stringWithFormat:@"module=%@,errid=%d,errmsg=%@",module,errId,errMsg];
+            NSLog(@"createRoom fail.%@",errinfo);
+        }];
+    }
+}
+
+
+- (void)updateViews:(CDVInvokedUrlCommand*)command
+{
+    
+}
+
+- (void)quit:(CDVInvokedUrlCommand*)command
+{
+    
+}
+
 
 @end
